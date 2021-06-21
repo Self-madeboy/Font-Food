0520d53fd20b05dd9766a2a5a0d386274d0775c9
Self-madeboy <1738203391@qq.com>
2021-06-21 23:19:24 +0800

    修æè修改本地地址

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 window.app = {
     /* 开发环境 */
-    // serverUrl: "http://localhost:8088",                                   // 接口服务接口地址
-    // paymentServerUrl: "http://192.168.1.3:8089",                            // 支付中心服务地址
-    // shopServerUrl: "http://localhost:8080/foodie-shop/",                  // 门户网站地址
-    // centerServerUrl: "http://localhost:8080/foodie-center/",              // 用户中心地址
-    // cookieDomain: "",                                                       // cookie 域
+    serverUrl: "http://localhost:8086",                                   // 接口服务接口地址
+    paymentServerUrl: "http://192.168.1.3:8089",                            // 支付中心服务地址
+    shopServerUrl: "http://localhost:8080/foodie-shop/",                  // 门户网站地址
+    centerServerUrl: "http://localhost:8080/foodie-center/",              // 用户中心地址
+    cookieDomain: "",                                                       // cookie 域
 
     /* 生产环境 */
-    serverUrl: "http://api.z.mukewang.com:8088/foodie-dev-api",                      // 接口服务接口地址
-    paymentServerUrl: "http://payment.t.mukewang.com/foodie-payment",       // 支付中心服务地址
-    shopServerUrl: "http://shop.z.mukewang.com:8080/foodie-shop/",                            // 门户网站地址
-    centerServerUrl: "http://center.z.mukewang.com:8080/foodie-center/",                        // 用户中心地址
-    cookieDomain: ".z.mukewang.com;",                                       // cookie 域
+    // serverUrl: "http://api.z.mukewang.com:8088/foodie-dev-api",                      // 接口服务接口地址
+    // paymentServerUrl: "http://payment.t.mukewang.com/foodie-payment",       // 支付中心服务地址
+    // shopServerUrl: "http://shop.z.mukewang.com:8080/foodie-shop/",                            // 门户网站地址
+    // centerServerUrl: "http://center.z.mukewang.com:8080/foodie-center/",                        // 用户中心地址
+    // cookieDomain: ".z.mukewang.com;",                                       // cookie 域
 
     ctx: "/foodie-shop",
 
@@ -35,7 +35,7 @@ window.app = {
 
     setCookie: function(name, value) {
         var Days = 365;
-        var exp = new Date(); 
+        var exp = new Date();
         exp.setTime(exp.getTime() + Days*24*60*60*1000);
         var cookieContent = name + "="+ encodeURIComponent (value) + ";path=/;";
         if (this.cookieDomain != null && this.cookieDomain != undefined && this.cookieDomain != '') {
@@ -99,7 +99,7 @@ window.app = {
                     // 删除主图在数组中的位置
                     foodieShopcart.splice(i, 1, tmpItem);
                 }
-            }   
+            }
             if (!isHavingItem) {
                 foodieShopcart.push(pendingItem);
             }
